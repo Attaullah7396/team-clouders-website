8527d990e4c881d7087461739c73abfe7cc31050
Mohammad Attaullah <awsumfriend7396@gmail.com>
2016-08-31 19:55:57 +0500

    email send done

--- Rex/assets/js/app.js
+++ Rex/assets/js/app.js
@@ -8,3 +8,15 @@ function goToGoogle(){
     document.getElementById("myForm").reset();
 }
 
+function sendEmail(){
+    var name = document.getElementById('contactName').value;
+    var userEmail = document.getElementById('contactEmail').value;
+    var content = document.getElementById('contactDetail').value;
+    emailjs.send("gmail","template_gCHF6oh5",{name: name,from:userEmail, text:content})
+        .then(function(response) {
+            console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+        }, function(err) {
+            console.log("FAILED. error=", err);
+        });
+}
+
